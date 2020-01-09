8d3f3dddb10098de2500b241aba98c5b6431b476
Kevin0z0 <644720213@qq.com>
2020-01-09 15:36:20 +0800

    转换编码
    解决编码不乱码和报错问题，有些无法转换的直接退出程序

---
+++ transcoding.py
@@ -0,0 +1,21 @@
+import chardet
+
+def judge(data):
+    return chardet.detect(data)["encoding"]
+
+def error(e,q=1):
+    input(e)
+    if q:
+        exit(0)
+
+def trans(path):
+    data = open(path, "rb").read()
+    coding = judge(data)
+    try:
+        arr = [i.rstrip() for i in data.decode(coding).split("\n")]
+        if len(arr) == 1:
+            return [i for i in arr[0].split("\r")]
+        return arr
+    except Exception as e:
+        print(e)
+        error("[!] 无法使用此文本,请使用utf8编码的文本")
